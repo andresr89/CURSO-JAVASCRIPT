262b57570c5a32c8e598980560313ec7b18eb591
Andres Rodriguez <andresarodriguezr89@gmail.com>
2024-08-26 20:56:33 -0500

    first commit

---
+++ hola-mundo.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = 'Woody'
+console.log (cajaDeAndy)
+
+// lo NO permitido
+
+let c ='Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+// lo Permitido
+
+let PrimerTrasteoDeAndy = 'Woody'
+let urlDelUsuario = 'htpp:/www.google.com'
+let idDelUsuario = '123456'
---
+++ tipos-de-datos.js
@@ -0,0 +1,38 @@
+// PRIMITIVOS
+
+// string
+let nomnre='Tere'
+
+//  number
+let edad= 25
+
+//  boolean
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+// undefined
+let noDefinido = undefined
+
+//  symbol
+let simboloUnico = Symbol ('único')
+
+// bigint
+let numeroGrane = 2n
+
+// COMPLEJOS
+
+// object
+let carro = {
+    marca : 'Tesla',
+    modelo : 'Model S'
+}
+
+// array
+let frutas = ['manzana', 'bananas', 'uvas']
+
+// function
+function  saludar (){}
+
+
